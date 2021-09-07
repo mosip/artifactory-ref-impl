6db8cea7e5ec69c92b5e4c391f7325ec46656aed
ckm007 <chandrakeshavmishra@gmail.com>
2021-09-07 14:32:14 +0000

    [MOSIP-17370] updated build version script to update the version in Dockerfile

--- artifacts/build_version.sh
+++ artifacts/build_version.sh
@@ -6,32 +6,64 @@ echo Script to fetch the timestamp for each required snapshot artifactory
 
 echo timestamp for auth adapter.
 
-curl -s https://oss.sonatype.org/content/repositories/snapshots/io/mosip/kernel/kernel-auth-adapter/$version/maven-metadata.xml | grep value | sed "s/.*<value>\([^<]*\)<\/value>.*/\1/" | sed -n '1p'
+a=$(curl -s https://oss.sonatype.org/content/repositories/snapshots/io/mosip/kernel/kernel-auth-adapter/$version/maven-metadata.xml | grep value | sed "s/.*<value>\([^<]*\)<\/value>.*/\1/" | sed -n '1p')
+
+sed -i "s/kernel-auth-adapter-.*.jar/kernel-auth-adapter-$a.jar/g" Dockerfile
+
+echo $a
 
 echo timestamp for msg91 sms jar
 
-curl -s https://oss.sonatype.org/content/repositories/snapshots/io/mosip/kernel/kernel-smsserviceprovider-msg91/$version/maven-metadata.xml | grep value | sed "s/.*<value>\([^<]*\)<\/value>.*/\1/" | sed -n '1p'
+a=$(curl -s https://oss.sonatype.org/content/repositories/snapshots/io/mosip/kernel/kernel-smsserviceprovider-msg91/$version/maven-metadata.xml | grep value | sed "s/.*<value>\([^<]*\)<\/value>.*/\1/" | sed -n '1p')
+
+sed -i "s/kernel-smsserviceprovider-msg91-.*.jar/kernel-smsserviceprovider-msg91-$a.jar/g" Dockerfile
+
+echo $a
 
 echo timestamp for ref-idobjectvalidator jar
 
-curl -s https://oss.sonatype.org/content/repositories/snapshots/io/mosip/kernel/kernel-ref-idobjectvalidator/$version/maven-metadata.xml | grep value | sed "s/.*<value>\([^<]*\)<\/value>.*/\1/" | sed -n '1p'
+a=$(curl -s https://oss.sonatype.org/content/repositories/snapshots/io/mosip/kernel/kernel-ref-idobjectvalidator/$version/maven-metadata.xml | grep value | sed "s/.*<value>\([^<]*\)<\/value>.*/\1/" | sed -n '1p')
+
+echo $a
+
+sed -i "s/kernel-ref-idobjectvalidator-.*.jar/kernel-ref-idobjectvalidator-$a.jar/g" Dockerfile
 
 echo timestamp for virus scanner jar 
 
-curl -s https://oss.sonatype.org/content/repositories/snapshots/io/mosip/kernel/kernel-virusscanner-clamav/$version/maven-metadata.xml | grep value | sed "s/.*<value>\([^<]*\)<\/value>.*/\1/" | sed -n '1p'
+a=$(curl -s https://oss.sonatype.org/content/repositories/snapshots/io/mosip/kernel/kernel-virusscanner-clamav/$version/maven-metadata.xml | grep value | sed "s/.*<value>\([^<]*\)<\/value>.*/\1/" | sed -n '1p')
+
+echo $a
+
+sed -i "s/kernel-virusscanner-clamav-.*.jar/kernel-virusscanner-clamav-$a.jar/g" Dockerfile
 
 echo timestamp for mock-sdk jar
 
-curl -s https://oss.sonatype.org/content/repositories/snapshots/io/mosip/mock/sdk/mock-sdk/$version/maven-metadata.xml | grep value | sed "s/.*<value>\([^<]*\)<\/value>.*/\1/" | sed -n '1p'
+a=$(curl -s https://oss.sonatype.org/content/repositories/snapshots/io/mosip/mock/sdk/mock-sdk/$version/maven-metadata.xml | grep value | sed "s/.*<value>\([^<]*\)<\/value>.*/\1/" | sed -n '1p')
+
+echo $a
+
+sed -i "s/mock-sdk-.*.jar/mock-sdk-$a.jar/g" Dockerfile
 
 echo timestamp for  biosdk client jar
 
-curl -s https://oss.sonatype.org/content/repositories/snapshots/io/mosip/biosdk/biosdk-client/$version/maven-metadata.xml | grep value | sed "s/.*<value>\([^<]*\)<\/value>.*/\1/" | sed -n '1p'
+a=$(curl -s https://oss.sonatype.org/content/repositories/snapshots/io/mosip/biosdk/biosdk-client/$version/maven-metadata.xml | grep value | sed "s/.*<value>\([^<]*\)<\/value>.*/\1/" | sed -n '1p')
+
+echo $a
+
+sed -i "s/biosdk-client-.*.jar/biosdk-client-$a.jar/g" Dockerfile
 
 echo timestamp for  demosdk client jar
 
-curl -s https://oss.sonatype.org/content/repositories/snapshots/io/mosip/demosdk/demosdk-client/$version/maven-metadata.xml | grep value | sed "s/.*<value>\([^<]*\)<\/value>.*/\1/" | sed -n '1p'
+a=$(curl -s https://oss.sonatype.org/content/repositories/snapshots/io/mosip/demosdk/demosdk-client/$version/maven-metadata.xml | grep value | sed "s/.*<value>\([^<]*\)<\/value>.*/\1/" | sed -n '1p')
+
+echo $a
+
+sed -i "s/demosdk-client-.*.jar/demosdk-client-$a.jar/g" Dockerfile
 
 echo timestamp for  child auth filter jar
 
-curl -s https://oss.sonatype.org/content/repositories/snapshots/io/mosip/authentication/authentication-childauthfilter-impl/$version/maven-metadata.xml | grep value | sed "s/.*<value>\([^<]*\)<\/value>.*/\1/" | sed -n '1p'
+a=$(curl -s https://oss.sonatype.org/content/repositories/snapshots/io/mosip/authentication/authentication-childauthfilter-impl/$version/maven-metadata.xml | grep value | sed "s/.*<value>\([^<]*\)<\/value>.*/\1/" | sed -n '1p')
+
+echo $a
+
+sed -i "s/authentication-childauthfilter-.*.jar/authentication-childauthfilter-$a.jar/g" Dockerfile
