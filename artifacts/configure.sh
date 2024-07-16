06c859b950e2a61d97c68b6b5c273c30c5054e51
HimajaDhanyamraju2 <dhanyamraju.himaja2@mindtree.com>
2024-07-16 15:29:08 +0530

    MOSIP-32454: Moving regclient stub-impl jars to artifactory
    Signed-off-by: HimajaDhanyamraju2 <dhanyamraju.himaja2@mindtree.com>

--- artifacts/configure.sh
+++ artifacts/configure.sh
@@ -10,6 +10,7 @@ set -e
 # 4. demosdk-lib zip creation
 # 5. Create resources zip for reg-client
 # 6. Create i18n and entity-spec bundles zip files for all the required modules
+# 7. registration-api-stub-impl zip creation
 
 echo biosdk client zip creation started
 zip -r -j ${biosdk_client_zip_path}/biosdk-client.zip ${biosdk_client_zip_path}/biosdk-client/*
@@ -103,4 +104,7 @@ echo master-templates zip creation started
 zip -r -j ${master_template_path}/master-templates.zip ${work_dir}/master-templates/*
 echo master-templates zip creation completed
 
-
+echo registration-api-stub-impl zip creation started
+zip -r -j ${registration_api_stub_impl_zip_path}/registration-api-stub-impl.zip ${registration_api_stub_impl_zip_path}/registration-api-stub-impl/*
+rm -rf ${registration_api_stub_impl_zip_path}/registration-api-stub-impl
+echo registration-api-stub-impl zip creation completed
