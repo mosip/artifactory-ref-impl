35ee670a74f1bddea1a269b6cd13072fc8a7a2df
Chandra Keshav Mishra <chandrakeshavmishra@gmail.com>
2026-09-08 20:28:42 +0530

    Restore files lost in accidental mass-deletion merge (PR #1153)

---
+++ README.md
@@ -0,0 +1,37 @@
+# artifactory-server
+
+This is a refeference implementation of artifactory service used within MOSIP. The artifactory service contains the dynamically loaded libraries and services. The current repository is a reference implementation of the artifactory service and it packages the necessary mocks, reference implementation to run the default version of MOSIP.
+
+Following are the artifacts which is being served by the service:
+  1. Auth-adapter.jar
+  2. Ref-Idobjectvalidator.jar
+  3. Biosdk.zip
+  4. hsm client
+  5. cache jar
+  6. sms-notification.jar
+  7. clamav-antivirus.jar
+
+All these artifacts are released as a part of the Mosip Release with some of them being taken from open sourced repository as per the need.
+
+## Deployment in K8 cluster with other MOSIP services:
+### Pre-requisites
+* Set KUBECONFIG variable to point to existing K8 cluster kubeconfig file:
+  * ```
+    export KUBECONFIG=~/.kube/<my-cluster.config>
+    ```
+### Install
+  ```
+    $ cd deploy
+    $ ./install.sh
+   ```
+### Delete
+  ```
+    $ cd deploy
+    $ ./delete.sh
+   ```
+### Restart
+  ```
+    $ cd deploy
+    $ ./restart.sh
+   ```
+
---
+++ artifacts/build_version.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+version=1.2.0.1-SNAPSHOT
+idp_auth_wrapper_version=0.9.0-SNAPSHOT
+esignet_auth_wrapper_version=1.0.0-SNAPSHOT
+
+echo Script to fetch the timestamp for each required snapshot artifactory
+
+echo timestamp for auth adapter.
+
+curl -s https://oss.sonatype.org/content/repositories/snapshots/io/mosip/kernel/kernel-auth-adapter/$version/maven-metadata.xml | grep value | sed "s/.*<value>\([^<]*\)<\/value>.*/\1/" | sed -n '1p'
+
+echo timestamp for msg91 sms jar
+
+curl -s https://oss.sonatype.org/content/repositories/snapshots/io/mosip/kernel/kernel-smsserviceprovider-msg91/$version/maven-metadata.xml | grep value | sed "s/.*<value>\([^<]*\)<\/value>.*/\1/" | sed -n '1p'
+
+echo timestamp for ref-idobjectvalidator jar
+
+curl -s https://oss.sonatype.org/content/repositories/snapshots/io/mosip/kernel/kernel-ref-idobjectvalidator/$version/maven-metadata.xml | grep value | sed "s/.*<value>\([^<]*\)<\/value>.*/\1/" | sed -n '1p'
+
+echo timestamp for virus scanner jar 
+
+curl -s https://oss.sonatype.org/content/repositories/snapshots/io/mosip/kernel/kernel-virusscanner-clamav/$version/maven-metadata.xml | grep value | sed "s/.*<value>\([^<]*\)<\/value>.*/\1/" | sed -n '1p'
+
+echo timestamp for mock-sdk jar
+
+curl -s https://oss.sonatype.org/content/repositories/snapshots/io/mosip/mock/sdk/mock-sdk/$version/maven-metadata.xml | grep value | sed "s/.*<value>\([^<]*\)<\/value>.*/\1/" | sed -n '1p'
+
+echo timestamp for  biosdk client jar
+
+curl -s https://oss.sonatype.org/content/repositories/snapshots/io/mosip/biosdk/biosdk-client/$version/maven-metadata.xml | grep value | sed "s/.*<value>\([^<]*\)<\/value>.*/\1/" | sed -n '1p'
+
+echo timestamp for  demosdk client jar
+
+curl -s https://oss.sonatype.org/content/repositories/snapshots/io/mosip/demosdk/demosdk/$version/maven-metadata.xml | grep value | sed "s/.*<value>\([^<]*\)<\/value>.*/\1/" | sed -n '1p'
+
+echo timestamp for  child auth filter jar
+
+curl -s https://oss.sonatype.org/content/repositories/snapshots/io/mosip/authentication/authentication-childauthfilter-impl/$version/maven-metadata.xml | grep value | sed "s/.*<value>\([^<]*\)<\/value>.*/\1/" | sed -n '1p'
+
+echo timestamp for IDP auth wrapper jar
+
+curl -s https://oss.sonatype.org/content/repositories/snapshots/io/mosip/idp/authentication-wrapper/${idp_auth_wrapper_version}/maven-metadata.xml | grep value | sed "s/.*<value>\([^<]*\)<\/value>.*/\1/" | sed -n '1p'
+
+
+echo timestamp for Esignet auth wrapper jar
+
+curl -s https://oss.sonatype.org/content/repositories/snapshots/io/mosip/idp/authentication-wrapper/${esignet_auth_wrapper_version}/maven-metadata.xml | grep value | sed "s/.*<value>\([^<]*\)<\/value>.*/\1/" | sed -n '1p'
